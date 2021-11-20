6ff1a77f6bce403d258701d52c193d0f87d67042
Dell <Dell@DESKTOP-KM53AHL>
2021-11-20 12:21:21 +0200

    test2

---
+++ TestBMS/src/com/BMS/uhia/Testing2.java
@@ -0,0 +1,19 @@
+package com.BMS.uhia;
+
+import utility.Family_Registration;
+
+public class Testing2 {
+//My Test comment 
+	public static void main(String[] args) {
+		// Test Commit 
+		Family_Registration fr = new Family_Registration();
+		fr.InvokeBrowser();
+		fr.Login_to_BMS("digital1","digital1@staging");
+		//fr.Search_for_Beneficiary_by_NID("20000000000032");
+		/*
+		fr.Add_new_Family("5000", "1000000002", "testmahmoud@yahoos.com", "Nasr city,Cairo,Egypty");
+		fr.teardown();
+		*/
+	}
+	
+}
